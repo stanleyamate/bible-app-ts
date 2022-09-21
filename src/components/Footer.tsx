cb1ab163361ac5c0034ee64f03bc0e28ae7349cf
stanleyamate <amatestanley08@gmail.com>
2022-09-21 11:58:42 +0100

    footer styled

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer className="min-h-full text-gray-600 bg-gray-300 p-3 flex justify-between">
+       <div>@bible-app</div>
+       <div>copyright@2022</div>
+      </footer>
+    </>
   )
 }
 
