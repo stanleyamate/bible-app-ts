32a190ae4ccc43c29606845c8d05c7c2bf689a67
stanleyamate <amatestanley08@gmail.com>
2022-09-22 17:48:39 +0100

    final update

--- src/components/Bible.tsx
+++ src/components/Bible.tsx
@@ -8,13 +8,13 @@ import Button from './Button';
 const Bible = () => {
 
   return (   <>
-                  <div className="card border bg-white flex flex-col justify-center shadow-lg p-3 rounded-md overflow-hidden relative">
-                    <img className="absolute -z-1 opacity-40 -right-40 h-52" src={bible} alt="" />
+                  <div className="card">
+                    {/* <img className="absolute -z-1 opacity-40 -right-40 h-52" src={bible} alt="" />
                     <h4 className="font-bold text-orange-400 self-start">KJV</h4>
                     <h5 className="font-bold">Lorem ipsum dolor sit amet</h5>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing</p>
-                    <Button className={"btn-blue"}>Read</Button>
-                    {/* <Animations /> */}
+                    <Button className={"btn-blue"}>Read</Button> */}
+                    <Animations />
                 </div>  
                 </>
   )
--- src/components/Skeletons/BibleSkeleton.tsx
+++ src/components/Skeletons/BibleSkeleton.tsx
@@ -3,10 +3,10 @@ import Skeleton from '@mui/material/Skeleton';
 export const Animations = () =>{
   return (
     <div className="w-[100%]">
-      <Skeleton variant="rounded" className="w-full" height={20}/>
-      <Skeleton animation="wave" className="w-[90%]"/>
-      <Skeleton animation="wave"  className="w-[90%]" height={15}/>
-      <Skeleton animation="wave"  className="w-[90%]" height={15}/>
+      <Skeleton variant="rounded" className="w-40" height={20}/>
+      <Skeleton animation="wave" className="w-[90%] mx-auto"/>
+      <Skeleton animation="wave"  className="w-[85%] mx-auto" height={15}/>
+      <Skeleton animation="wave"  className="w-[85%] mx-auto" height={15}/>
       <Skeleton animation="wave"  width={80} height={50}/>
     </div>
   );}
